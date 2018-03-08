b5e18e2fab8fe03ceb11ebafebf4f1c8e2ec760b
nd <git@notandy.de>
2018-03-08 12:06:46 +0100

    automaticly refreshing spotify auth token, more verbose output

--- skipify.py
+++ skipify.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-import pprint
 import sys
 import time
 
@@ -9,21 +8,28 @@ import spotipy.util as util
 
 from config import *
 
-token = util.prompt_for_user_token(api_user, scope='user-read-currently-playing user-read-playback-state user-modify-playback-state', client_id=api_id, client_secret=api_secret, redirect_uri='https://localhost/asd' )
-
-if token:
-	sp = spotipy.Spotify(auth=token)
-	while True:
-		playing = sp.currently_playing()
-		if not playing.get('is_playing'):
-			time.sleep(30)
-			continue
-		artists = playing.get('item',{}).get('artists',[])
-		for a in artists:
-			if a.get('name') in blocklist:
-				print('{} matched block list, skipping'.format(a.get('name')))
-				sp.next_track()
-				break
-		time.sleep(7)
-else:
-	print("Can't get token for", api_user)
+print('Skipping the following artists: {}'.format(blocklist.get('artist.name')))
+while True:
+	try:
+		token = util.prompt_for_user_token(api_user, scope='user-read-currently-playing user-read-playback-state user-modify-playback-state', client_id=api_id, client_secret=api_secret, redirect_uri='https://localhost/asd' )
+		if token:
+			print('Refreshed spotify auth token')
+			sp = spotipy.Spotify(auth=token)
+			while True:
+				playing = sp.currently_playing()
+				if not playing.get('is_playing'):
+					time.sleep(30)
+					continue
+				song = playing.get('item',{})
+				artists = song.get('artists',[])
+				for artist in artists:
+					if artist.get('name') in blocklist.get('artist.name', []):
+						print('"{}" matched block list, skipping "{}"'.format(artist.get('name'), song.get('name')))
+						sp.next_track()
+						break
+				time.sleep(7)
+		else:
+			print("Can't get token for", api_user)
+			exit(1)
+	except spotipy.client.SpotifyException:
+		pass
